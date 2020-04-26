af75b01221a357a81526d3acad75d99e639e9bde
atsu0131 <atsushi.isai131@gmail.com>
2020-04-26 18:31:18 +0900

    add

--- app/handler/handler.go
+++ app/handler/handler.go
@@ -2,8 +2,16 @@ package handler
 
 import (
     "net/http"
-    "github.com/labstack/echo"
+		"github.com/labstack/echo"
+    "database/sql"
+    _ "github.com/go-sql-driver/mysql"
 )
+
+// JSON用の構造体を定義
+type Task struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+}
 
 func MainPage() echo.HandlerFunc {
     return func(c echo.Context) error {     //c をいじって Request, Responseを色々する 
@@ -17,4 +25,46 @@ func MainPage() echo.HandlerFunc {
 			return c.JSON(http.StatusOK, jsonMap)
 
     }
+}
+
+
+func dbConnect() *sql.DB {
+	// DB接続処理
+	db, err := sql.Open("mysql", "root:password@tcp(test_db)/test_todo")
+	if err != nil {
+			panic(err.Error())
+	}
+	return db
+}
+
+func getRows(db *sql.DB) *sql.Rows {
+	rows, err := db.Query("SELECT id,name FROM tasks")
+	if err != nil {
+			panic(err.Error())
+	}
+	return rows
+}
+
+func GetTasks() echo.HandlerFunc {
+	return func(c echo.Context) error {
+		// DB接続
+		db := dbConnect()
+		defer db.Close()
+		rows := getRows(db)
+		defer rows.Close()
+
+		task := Task{}
+		var results []Task
+		for rows.Next() {
+				err := rows.Scan(&task.ID, &task.Name)
+				if err != nil {
+						panic(err.Error())
+				} else {
+						results = append(results, task)
+				}
+		}
+		return c.JSON(http.StatusOK, results)
+	}
+
+
 }
--- app/main.go
+++ app/main.go
@@ -1,102 +1,68 @@
 package main
 
 import (
-      //  "net/http"
-			 "github.com/labstack/echo"
-			 "github.com/labstack/echo/middleware"
-			 "app/handler"
+    "database/sql"
+    // "fmt"
+    "net/http"
+		"app/handler"
+    _ "github.com/go-sql-driver/mysql"
+    "github.com/labstack/echo"
 )
 
-func main () {
-		e := echo.New()
-		 
-		// Middleware
-		e.Use(middleware.Logger())
-		e.Use(middleware.Recover())
-		 
-		//  // ルーティング
-		e.GET("/", handler.MainPage())
-    // Routes
-    e.GET("/users", handler.ListUser)
+// JSON用の構造体を定義
+type Task struct {
+    ID   int    `json:"id"`
+    Name string `json:"name"`
+}
+
+func main() {
+    e := echo.New()
+		e.GET("/tasks", getTasks)
+		// e.GET("/", handler.GetTasks)
+		
+    // e.GET("/users", handler.ListUser)
     // e.GET("/users/:id", handler.GetUser)
-    e.POST("/users", handler.CreateUser)
+    // e.POST("/users", handler.CreateUser)
     // e.PUT("/users/:id", handler.UpdateUser)
     // e.DELETE("/users/:id", handler.DeleteUser)
 
-	//  // サーバー起動
-	// e.Startの中はdocker-composeのgoコンテナで設定したportsを指定してください。
-		e.Logger.Fatal(e.Start(":8082"))
+    e.Start(":8082")
 }
 
-// package main
+func dbConnect() *sql.DB {
+    // DB接続処理
+    db, err := sql.Open("mysql", "root:password@tcp(test_db)/test_todo")
+    if err != nil {
+        panic(err.Error())
+    }
+    return db
+}
 
-// import (
-//     "database/sql"
-//     "fmt"
-//     "net/http"
+func getRows(db *sql.DB) *sql.Rows {
+    rows, err := db.Query("SELECT id,name FROM tasks")
+    if err != nil {
+        panic(err.Error())
+    }
+    return rows
+}
 
-//     _ "github.com/go-sql-driver/mysql"
-//     "github.com/labstack/echo"
-// )
+func getTasks(c echo.Context) error {
+    // DB接続
+    db := dbConnect()
+    defer db.Close()
+    rows := getRows(db)
+    defer rows.Close()
 
-// // JSON用の構造体を定義
-// type Task struct {
-//     ID   int    `json:"id"`
-//     Name string `json:"name"`
-// }
+    task := Task{}
+    var results []Task
+    for rows.Next() {
+        err := rows.Scan(&task.ID, &task.Name)
+        if err != nil {
+            panic(err.Error())
+        } else {
+            results = append(results, task)
+        }
+    }
 
-// func main() {
-//     e := echo.New()
-//     // e.GET("/tasks", getTasks)
-// 		// e.Start(":8082")
-//     //  e.GET("/", func(c echo.Context) error {
-//     //     // return c.String(http.StatusOK, "Hello, World!")
-
-//     //  })
-// 		//  e.Logger.Fatal(e.Start(":8082"))
-		
-// 		e.GET("/", func(c echo.Context) error {
-// 			return c.String(http.StatusOK, "Hello, World!")
-// 		})
-// 		e.Logger.Fatal(e.Start(":8082"))
-
-    
-// }
-
-// func dbConnect() *sql.DB {
-//     // DB接続処理
-//     db, err := sql.Open("mysql", "root:password@tcp(test_db)/test_todo")
-//     if err != nil {
-//         panic(err.Error())
-//     }
-//     return db
-// }
-
-// func getRows(db *sql.DB) *sql.Rows {
-//     rows, err := db.Query("SELECT id,name FROM tasks")
-//     if err != nil {
-//         panic(err.Error())
-//     }
-//     return rows
-// }
-
-// func getTasks(c echo.Context) error {
-//     // DB接続
-//     db := dbConnect()
-//     defer db.Close()
-//     rows := getRows(db)
-//     defer rows.Close()
-
-//     task := Task{}
-//     var results []Task
-//     for rows.Next() {
-//         err := rows.Scan(&task.ID, &task.Name)
-//         if err != nil {
-//             panic(err.Error())
-//         } else {
-//             results = append(results, task)
-//         }
-//     }
-
-//     return c.JSON(http.StatusOK, results)
-// }
+    return c.JSON(http.StatusOK, results)
+}
